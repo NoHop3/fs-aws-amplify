ce3aa4b8a6ad02eadae29ab4ea7b3c7cd5469ee8
NoHop3 <u_sux@abv.bg>
2022-06-28 10:11:53 +0200

    Working towards a global theming

--- src/components/header/Logo.styled.ts
+++ src/components/header/Logo.styled.ts
@@ -1,5 +1,4 @@
 import styled from "styled-components";
-import React from "react";
 
 export const Logo = styled.img`
   display: block;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,105 +1,13 @@
 import React from 'react'
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 
 export default function Home() {
   return (
-    <div>
-      {" "}
-      Home Lorem ipsum dolor sit amet consectetur adipisicing elit. Id nostrum
-      animi autem dolor provident, esse cum dignissimos aspernatur, vitae
-      voluptates sed alias quibusdam eum eos asperiores culpa tenetur in
-      voluptas! Ut asperiores officiis facilis blanditiis illum ullam iusto unde
-      assumenda, quam quasi cum maxime cupiditate beatae quo atque ducimus, non,
-      aut ea. Sed nihil, omnis ipsam amet saepe vitae aperiam adipisci suscipit
-      ab itaque, vero aut? Error voluptatum asperiores non nihil eveniet iure
-      possimus enim rem, consectetur reiciendis placeat veritatis perspiciatis,
-      et aliquam eos tempora dolorem eaque pariatur nulla voluptates
-      reprehenderit. Numquam nostrum, ab accusamus eos aut provident ex qui
-      ipsum quae nesciunt minima pariatur rem unde consequatur! Dolor, optio
-      quia fugiat soluta laboriosam sapiente et. Deleniti consequuntur mollitia
-      inventore aspernatur labore sunt excepturi facilis voluptate, praesentium
-      porro ut aliquam ea dolor nesciunt temporibus nisi. Eius alias dicta, quo
-      ad corporis doloribus veritatis tempore vero consectetur accusantium est,
-      nostrum explicabo eum nisi! Animi molestias aliquid minima at sed quaerat,
-      cupiditate omnis eligendi possimus. Dolore odit vitae non eius maiores
-      facere magnam. Qui fuga temporibus earumasdadsasd laborum soluta harum
-      tempore sequi maiores veritatis alias odio culpa nemo quaerat, nihil, modi
-      nulla dolorum incidunt. Nesciunt, at inventore voluptatum, sint impedit
-      consequatur, ex itaque ullam facilis earum adipisci reiciendis quo qui
-      rem! Deleniti ea aliquam amet voluptatibus maxime autem excepturi
-      molestias nisi impedit reprehenderit laborum consequuntur accusamus magnam
-      quae vel, nihil veritatis dolores ipsa sint tempore labore ratione nulla
-      consequatur atque? In repellat facere voluptates omnis quisquam aliquam
-      corrupti esse officia, ipsam, quibusdam earum cupiditate dolores
-      accusantium dignissimos molestiae quae ab officiis tempore ducimus qui
-      tenetur quod! Quod, aliquid est perspiciatis itaque sequi laboriosam
-      quibusdam voluptatem aspernatur? Suscipit animi, non tempora perferendis
-      ut velit nihil reiciendis quasi ex omnis? Velit earum magni placeat!
-      Voluptatibus quis, cupiditate asperiores omnis mollitia quas porro ratione
-      cum ad soluta repellat eius tempora autem, quam natus doloremque
-      perspiciatis quaerat nobis laborum ea. Praesentium nam eligendi hic itaque
-      illum, ad esse optio doloremque, reiciendis vitae, ut dolore fuga! Quis
-      amet, ea adipisci odio vero debitis, aperiam consequuntur labore vel
-      deserunt facere similique eius fugit cupiditate explicabo beatae. In, hic
-      deleniti eligendi quasi animi, quod ut vel laboriosam earum ipsa accusamus
-      nostrum nihil reiciendis rerum quae quam consequatur excepturi
-      exercitationem. Velit quod iure architecto incidunt natus assumenda
-      perferendis aspernatur ut, quaerat cumque magnam corporis rerum reiciendis
-      odit ex, deleniti ab sapiente impedit quam quia tempora ducimus recusandae
-      repudiandae illum. Aliquid debitis, ut ducimus eum nam quisquam? Eius cum,
-      tempore suscipit distinctio voluptatum culpa obcaecati animi perferendis
-      non, libero doloribus cupiditate. Obcaecati deleniti impedit sequi,
-      dolorum, nisi eum doloribus sunt quod cumque a non maxime libero aliquid
-      tempora modi nostrum distinctio asperiores ad maiores labore! Sunt,
-      consequatur fugit! Magnam voluptatibus reprehenderit, earum asperiores
-      quidem, repellat et dolores blanditiis aperiam dolor magni dicta corrupti!
-      Perferendis iure quibusdam repellat, quis ipsam tempore architecto
-      reiciendis, et dicta quos accusamus exercitationem corporis, blanditiis in
-      consectetur eligendi accusantium! Ipsum quis perspiciatis ex perferendis
-      laboriosam, dignissimos eveniet minus voluptas nisi esse magni non at
-      provident neque nostrum saepe vitae placeat? Aut tenetur recusandae dolor
-      explicabo nobis vel fuga cumque, sapiente doloremque magni, neque aliquam
-      quisquam itaque suscipit ducimus ex perferendis odio accusantium sunt quo.
-      Soluta autem vel mollitia temporibus quos voluptates magni sed eligendi
-      incidunt, provident facilis libero voluptatibus culpa magnam voluptas,
-      reprehenderit fuga itaque nemo enim ex repellat commodi. Perferendis
-      magnam corporis rerum reiciendis odit ex, deleniti ab sapiente impedit
-      quam quia tempora ducimus recusandae repudiandae illum. Aliquid debitis,
-      ut ducimus eum nam quisquam? Eius cum, tempore suscipit distinctio
-      voluptatum culpa obcaecati animi perferendis non, libero doloribus
-      cupiditate. Obcaecati deleniti impedit sequi, dolorum, nisi eum doloribus
-      sunt quod cumque a non maxime libero aliquid tempora modi nostrum
-      distinctio asperiores ad maiores labore! Sunt, consequatur fugit! Magnam
-      voluptatibus reprehenderit, earum asperiores quidem, repellat et dolores
-      blanditiis aperiam dolor magni dicta corrupti! Perferendis iure quibusdam
-      repellat, quis ipsam tempore architecto reiciendis, et dicta quos
-      accusamus exercitationem corporis, blanditiis in consectetur eligendi
-      accusantium! Ipsum quis perspiciatis ex perferendis laboriosam,
-      dignissimos eveniet minus voluptas nisi esse magni non at provident neque
-      nostrum saepe vitae placeat? Aut tenetur recusandae dolor explicabo nobis
-      vel fuga cumque, sapiente doloremque magni, neque aliquam quisquam itaque
-      suscipit ducimus ex perferendis odio accusantium sunt quo. Soluta autem
-      vel mollitia temporibus quos voluptates magni sed eligendi incidunt,
-      provident facilis libero voluptatibus culpa magnam voluptas, reprehenderit
-      fuga itaque nemo enim ex repellat commodi. Perferendis magnam corporis
-      rerum reiciendis odit ex, deleniti ab sapiente impedit quam quia tempora
-      ducimus recusandae repudiandae illum. Aliquid debitis, ut ducimus eum nam
-      quisquam? Eius cum, tempore suscipit distinctio voluptatum culpa obcaecati
-      animi perferendis non, libero doloribus cupiditate. Obcaecati deleniti
-      impedit sequi, dolorum, nisi eum doloribus sunt quod cumque a non maxime
-      libero aliquid tempora modi nostrum distinctio asperiores ad maiores
-      labore! Sunt, consequatur fugit! Magnam voluptatibus reprehenderit, earum
-      asperiores quidem, repellat et dolores blanditiis aperiam dolor magni
-      dicta corrupti! Perferendis iure quibusdam repellat, quis ipsam tempore
-      architecto reiciendis, et dicta quos accusamus exercitationem corporis,
-      blanditiis in consectetur eligendi accusantium! Ipsum quis perspiciatis ex
-      perferendis laboriosam, dignissimos eveniet minus voluptas nisi esse magni
-      non at provident neque nostrum saepe vitae placeat? Aut tenetur recusandae
-      dolor explicabo nobis vel fuga cumque, sapiente doloremque magni, neque
-      aliquam quisquam itaque suscipit ducimus ex perferendis odio accusantium
-      sunt quo. Soluta autem vel mollitia temporibus quos voluptates magni sed
-      eligendi incidunt, provident facilis libero voluptatibus culpa magnam
-      voluptas, reprehenderit fuga itaque nemo enim ex repellat commodi.
-      Perferendis
-    </div>
+    <Box sx={{ textAlign: "center" }}>
+      <Typography variant='h6' sx={{ my: 2 }}>
+        Home
+      </Typography>
+    </Box>
   );
 }
