e9c6c490e087780af5f54d86f79d282657b6c467
NoHop3 <u_sux@abv.bg>
2023-02-26 17:44:42 +0100

    Fix warnings

--- src/pages/manufacturers/manufacturers.tsx
+++ src/pages/manufacturers/manufacturers.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 
-import { API, graphqlOperation } from "aws-amplify";
-import { listManufacturers } from "../../graphql/queries";
-import { Manufacturer } from "../../utils/typescript/types";
+// import { API, graphqlOperation } from "aws-amplify";
+// import { listManufacturers } from "../../graphql/queries";
+// import { Manufacturer } from "../../utils/typescript/types";
 
 export const _Manufacturers = () => {
   // const [manufacturers, setManufacturers] = useState([]);
